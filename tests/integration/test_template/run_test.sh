eeff93d786c5c424152e6e741f58d0a658941dbc
sfischer <30040612+yexilein@users.noreply.github.com>
2016-06-09 09:48:37 +0000

    MINOR -----
    Modified `test_template` for integration test to facilitate cloning for writing new tests.

---
+++ tests/integration/test_template/run_test.sh
@@ -0,0 +1,5 @@
+
+# modify this script at well (potentially using external scripts to analyze data)
+
+cd tRNA_consumption
+./test "input/params.in"
---
+++ tests/integration/test_template/test.cpp
@@ -0,0 +1,36 @@
+
+// ==================
+//  General Includes
+// ==================
+//
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE tRNAConsumption
+#include <boost/test/unit_test.hpp>
+// #include <boost/test/floating_point_comparison.hpp>
+
+// ==================
+//  Project Includes
+// ==================
+//
+#include "simulation.h"
+
+class SimulationFixture
+{
+public:
+  SimulationFixture (void)
+    : simulation ("input/params.in")
+  {}
+
+  Simulation simulation;
+};
+
+BOOST_FIXTURE_TEST_SUITE (BaseTests, SimulationFixture)
+ 
+BOOST_AUTO_TEST_CASE (TestName)
+{
+  simulation.run();
+  // Use any basic boost test such as BOOST_TEST_EQUAL
+  // or write any information to file to be used by run_test.sh
+}
+ 
+BOOST_AUTO_TEST_SUITE_END()
