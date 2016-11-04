4e9001def0288316628d21cd85d1c04dc2c95c15
sfischer <30040612+yexilein@users.noreply.github.com>
2016-11-04 14:47:42 +0000

    - added parameter files and injection files for two different concentrations of tetracyclin.  - increased rate of formation of Okazaki fragments  - created output subfolders for each experiment  - adapted post treatment to take into account all experiments and improved plot for RNA levels  - completed run.sh script

--- application_note/post_treatment.R
+++ application_note/post_treatment.R
@@ -4,9 +4,13 @@ scripts = c("load_concentrations.R", "load_replication.R", "plot_concentrations.
 
 for (f in scripts) source(file.path(scripts_path, f))
 
-input_dir = "output"
+input_path = "output"
+## subdir corresponding to "standard" simulation must appear first
+input_subdirs = c("normal","injection1","injection2");
+input_dir = file.path(input_path, input_subdirs)
 output_dir = "results"
-time = c(0,500);
+min_time = 0
+max_time = 600
 data_conc = "concentrations.out"
 data_dna = "chromosome.out"
 
@@ -14,8 +18,8 @@ my_pdf = function (filename, w, h) {
     pdf(file.path(output_dir,filename), width=w, height=h);
 }
 
-data = load_replication(file.path(input_dir, data_dna));
-data = data[[time[2]+1]];
+data = load_replication(file.path(input_dir[1], data_dna));
+data = data[[max_time+1]];
 min = data$sense[[1]][2]-10;
 max = data$sense[[1]][2]+1000;
 my_pdf('DNA_zoom.pdf'); plot_linear_DNA(data, min, max); dev.off();
@@ -29,9 +33,10 @@ par(mfrow=c(2,2),
 plot_DNA (data); 
 title ("Replication status");
 
-data = load_concentrations(file.path(input_dir, data_conc));
-data = data[(data['time']>=time[1]) & (data['time']<=time[2]),];
-time = data['time'];
+## load concentration information
+data = load_concentrations(file.path(input_dir[1], data_conc));
+data = data[(data['time']>=min_time) & (data['time']<=max_time),];
+time = data['time'] / 60;
 
 ## plot dNTPs
 plot_concentrations(time, data[c('dATP','dCTP','dGTP','dTTP')]); 
@@ -41,10 +46,20 @@ title("dNTP levels");
 early = c('U1','U4','U5','U10','U13')
 late = c('U726','U727','U728','U733','U734')
 rna_sums = cbind(rowSums(data[early]), rowSums(data[late]))
-plot_concentrations(time, data[c(early,late)]);
+colors = rep(rainbow(2), each = length(early))
+plot_concentrations(time, data[c(early,late)], colors=colors);
 title("RNA levels");
 
 ## plot proteins
-plot_concentrations(time, data[c('protein_tracker')]);
+protein_data= data['protein_tracker']
+## retrieve information from other runs
+if (length(input_dir) > 1) {
+    for (i in 2:length(input_dir)) {
+        conc = load_concentrations(file.path(input_dir[i], data_conc))
+        conc = conc[(conc['time']>=min_time) & (conc['time']<=max_time),];
+        protein_data = cbind(protein_data, conc['protein_tracker'])
+    }
+}
+plot_concentrations(time, protein_data);
 title("Protein levels");
 dev.off();
--- application_note/run.sh
+++ application_note/run.sh
@@ -4,6 +4,14 @@
 SRC_PATH=/home/sfischer/simulateur/myBacteria/src
 EXE_NAME=simulator
 
-PARAMS_IN=input/params.in
+PARAMS_IN="input/params.in input/params_injection1.in input/params_injection2.in"
 
-${SRC_PATH}/${EXE_NAME} ${PARAMS_IN}
+LOG_FILE=log.txt
+
+rm -f $LOG_FILE
+for FILE in $PARAMS_IN
+do
+    ${SRC_PATH}/${EXE_NAME} ${FILE} | tee -a $LOG_FILE
+done
+
+Rscript post_treatment.R | tee -a $LOG_FILE
