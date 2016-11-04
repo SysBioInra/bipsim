26982235c72a9e76e3ab163bc1caf96fb81fc8b9
sfischer <30040612+yexilein@users.noreply.github.com>
2016-11-04 14:48:22 +0000

    changed the way colors are handled in the standard concentration plot

--- scripts/plot_concentrations.R
+++ scripts/plot_concentrations.R
@@ -1,18 +1,18 @@
 
-plot_concentrations = function(time, concentrations, filename = "", leg = "") {
+plot_concentrations = function(time, concentrations, colors = c(), filename = "") {
     if (filename != "") { pdf(filename, width=7, height=5); }
 
     time = t(time); concentrations = t(concentrations);
     nb_lines = dim(concentrations)[1];
-    colors = rainbow(nb_lines)
+    if (length(colors)==0) { colors = rainbow(nb_lines) }
     plot_range = c(min(concentrations), max(concentrations))
 
     plot(time, concentrations[1,], type = "l", col = colors[1], ylim = plot_range, xlab = "time", ylab = "Number of molecules");
 
-    if (nb_lines> 1)
+    if (nb_lines> 1) {
         for (i in 2:nb_lines)
-            lines(time, concentrations[i,], col=colors[i]);
+        { lines(time, concentrations[i,], col=colors[i]); }
+    }
 
-    if (leg != "") { legend("topright", legend=leg, col=colors, lty=1) }
     if (filename != "") { dev.off(); }
 }
