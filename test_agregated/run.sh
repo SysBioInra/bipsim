674ef145fe0f74622b33227c3e786167ffb2d81c
sfischer <30040612+yexilein@users.noreply.github.com>
2017-01-20 16:44:23 +0000

    Added a new test directory with a simulation including agregated reactions

---
+++ test_agregated/run.sh
@@ -0,0 +1,10 @@
+
+#!/bin/sh
+
+SRC_PATH=/home/sfischer/simulateur/myBacteria/src
+EXE_NAME=simulator
+
+PARAMS_IN=input/params.in
+
+${SRC_PATH}/${EXE_NAME} ${PARAMS_IN}
+#valgrind --tool=callgrind ${SRC_PATH}/${EXE_NAME} ${PARAMS_IN}
