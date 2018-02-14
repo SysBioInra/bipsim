5115430591ae38ab079a7aebcd3dbdd29f81ff8c
sfischer <30040612+yexilein@users.noreply.github.com>
2018-02-14 11:04:19 +0000

    Finished refactoring GeneReader and added a unit test.

--- data/src/GeneReader.py
+++ data/src/GeneReader.py
@@ -3,26 +3,86 @@
 import csv
 
 
-class Gene:
-    def __init__(self, name, bsu, position, rbs, sense, aa_seq, TUs):
-        self.name = name
-        self.bsu = bsu
-        self.start = position[0]
-        self.end = position[1]
-        self.rbs_start = rbs[0]
-        self.rbs_end = rbs[1]
-        self.sense = sense
-        self.TUs = TUs
-        aas = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
-               'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
-        self.aa_composition = []
-        # we do not count initial fMet
-        aa_seq = aa_seq[1:]
-        for aa in aas:
-            self.aa_composition.append(aa_seq.count(aa))
+class GeneReader(object):
 
-    def name_and_size(self):
-        return '{} [{}:{}]'.format(self.name, self.rbs_start, self.end)
+    def __init__(self, input_stream, dna_length, log_stream=None):
+        parser = csv.reader(input_stream, delimiter='\t')
+        header = next(parser)
+        indices = ColumnIndices(header)
+        self.genes = []
+        self._no_RBS = []
+        self._invalid_RBS = []
+        self._long_RBS = []
+        self._invalid_start = []
+        self._invalid_stop = []
+        self._invalid_sequence = []
+        self._invalid_aa_seq = []
+        for row in parser:
+            if row[indices.type] != 'CDS':
+                continue
+            self.gene = Gene(row, indices, dna_length)
+            self._check_gene()
+            if self.gene.is_usable():
+                self.gene.set_rbs_at_valid_location()
+                self.genes.append(self.gene)
+        self._log_errors(log_stream)
+
+    def _check_gene(self):
+        self._check_rbs()
+        self._check_sequence()
+
+    def _check_rbs(self):
+        if self.gene.is_undefined_rbs():
+            self._no_RBS.append(self.gene.name)
+            return
+        if self.gene.is_rbs_after_start_codon():
+            self._invalid_RBS.append(self.gene.name)
+        if self.gene.is_rbs_too_far_before_stop_codon():
+            self._long_RBS.append(self.gene.name)
+
+    def _check_sequence(self):
+        if not self.gene.has_valid_sequence_length():
+            self._invalid_sequence.append(self.gene.name)
+        if not self.gene.has_valid_start_codon():
+            self._invalid_start.append(self.gene.name)
+        if not self.gene.has_valid_stop_codon():
+            self._invalid_stop.append(self.gene.name)
+        if self.gene.contains_premature_stop_codon():
+            self._invalid_sequence.append(self.gene.name)
+        if not self.gene.is_aa_sequence_consistent_with_sequence():
+            self._invalid_aa_seq.append(self.gene.name)
+
+    def _log_errors(self, log_stream):
+        if log_stream:
+            log_stream.write(self._rbs_errors() + '\n\n'
+                             + self._sequence_errors())
+
+    def _rbs_errors(self):
+        return '\n\n'.join((
+            self._rbs_error_msg('no RBS info', self._no_RBS),
+            self._rbs_error_msg('invalid RBS info', self._invalid_RBS),
+            self._rbs_error_msg('RBS far away from gene', self._long_RBS)
+            ))
+
+    def _rbs_error_msg(self, error_type, error_list):
+        return self._error_msg(
+            error_type + ', RBS was set at 21 bases from start codon',
+            error_list
+            )
+
+    def _error_msg(self, error_type, error_list):
+        return 'Genes with ' + error_type + ': ' + ', '.join(error_list) + '.'
+
+    def _sequence_errors(self):
+        return '\n\n'.join((
+            self._error_msg('invalid stop codon (they were excluded)',
+                            self._invalid_stop),
+            self._error_msg('invalid start codon', self._invalid_start),
+            self._error_msg('invalid length or containing a stop codon '
+                            '(they were excluded)', self._invalid_sequence),
+            self._error_msg('invalid aa sequence (they were excluded)',
+                            self._invalid_aa_seq)
+            ))
 
 
 class ColumnIndices(object):
@@ -41,127 +101,117 @@ class ColumnIndices(object):
         self.type = header.index('gene_category')
 
 
-class GeneReader:
+class Gene(object):
+    aas = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
+           'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
 
-    def __init__(self, input_stream, dna_length, log_stream=None):
-        parser = csv.reader(input_stream, delimiter='\t')
-        header = next(parser)
-        indices = ColumnIndices(header)
-        self.genes = []
-        self._no_RBS = []
-        self._invalid_RBS = []
-        self._long_RBS = []
-        invalid_start = []
-        invalid_stop = []
-        invalid_sequence = []
-        invalid_aa_seq = []
-        for r in parser:
-            if r[indices.type] != 'CDS':
-                continue
-            name, bsu, position, sense = \
-                self._extract_base_info(indices, dna_length)
-            rbs = self._extract_rbs(indices, position, dna_length)
+    def __init__(self, row, indices, dna_length):
+        self._indices = indices
+        self._dna_length = dna_length
+        self.name = self.bsu = self.sense = None
+        self.start = self.end = self.rbs_start = self.rbs_end = None
+        self.sequence = self.aa_seq = self.TUs = None
+        self.aa_composition = []
+        self._extract_row(row)
 
-            # check sequence
-            sequence = r[indices.sequence].strip()
-            aa_seq = r[indices.aa_seq].strip()
-            assert(len(sequence) == (position[1] - position[0] + 1))
-            if not self._is_valid_sequence(sequence):
-                invalid_sequence.append(name)
-                continue
-            if (len(sequence) != 3*(len(aa_seq)+1)):
-                invalid_aa_seq.append(name)
-                continue
-            if not self._is_valid_start_codon(sequence):
-                invalid_start.append(name)
-            if not self._is_valid_stop_codon(sequence):
-                invalid_stop.append(name)
-                continue
+    def _extract_row(self, row):
+        self.name = row[self._indices.name]
+        self.bsu = row[self._indices.bsu]
+        self.sense = int(row[self._indices.sense])
+        self._extract_position(row)
+        self._extract_rbs(row)
+        self._extract_sequence(row)
+        self._extract_aa_seq(row)
+        self._extract_TUs(row)
 
-            # read TUs
-            if (r[indices.TU] != ''):
-                TUs = map(str.strip, r[indices.TU].split(','))
-            else:
-                TUs = []
-
-            # create gene
-            self.genes.append(Gene(name, bsu, position, rbs, sense,
-                                   aa_seq, TUs))
-
-        # log all relevant information
-        if log_stream:
-            msg = 'Genes with no RBS info, ' \
-                  + 'RBS was set at 21 bases from start codon: '
-            log_stream.write(msg + ', '.join(no_RBS) + '.\n\n')
-            msg = 'Genes with invalid RBS info, RBS was set' \
-                  + ' at 21 bases from start codon: '
-            log_stream.write(msg + ', '.join(invalid_RBS) + '.\n\n')
-            msg = 'Genes with RBS far away from gene, RBS was set' \
-                  + ' at 21 bases from start codon: '
-            log_stream.write(msg + ', '.join(long_RBS) + '.\n\n')
-            msg = 'Genes with invalid stop codon (they were excluded): '
-            log_stream.write(msg + ', '.join(invalid_stop) + '.\n\n')
-            msg = 'Genes with invalid start codon: '
-            log_stream.write(msg + ', '.join(invalid_start) + '.\n\n')
-            msg = ('Genes with invalid length or containing a stop codon '
-                   '(they were excluded): ')
-            log_stream.write(msg + ', '.join(invalid_sequence) + '.\n\n')
-            msg = 'Genes with invalid aa sequence (they were excluded): '
-            log_stream.write(msg + ', '.join(invalid_aa_seq) + '.\n\n')
-
-    def _get_base_info(self, indices):
-        name = r[indices.name]
-        bsu = r[indices.bsu]
-        position = [int(r[indices.start]), int(r[indices.end])]
+    def _extract_position(self, row):
+        position = [int(row[self._indices.start]), int(row[self._indices.end])]
         assert(position[0] < position[1])
-        sense = int(r[indices.sense])
-        if sense != 1:
-            position = self._invert_position(position, dna_length)
-        return name, bsu, position, sense
+        if self.is_antisense():
+            position = self._invert_position(position)
+        self.start = position[0]
+        self.end = position[1]
 
-    def _invert_position(self, position, dna_length):
-        return [dna_length - position[0] + 1, dna_length - position[1] + 1]
+    def is_antisense(self):
+        return self.sense != 1
 
-    def _extract_rbs(self, indices, position, dna_length):
-        [int(r[indices.rbs_start]), int(r[indices.rbs_end])]
-        if self._is_undefined_rbs(rbs):
-            no_RBS.append(name)
-            rbs[0] = self._default_rba_start(position)
-        else:
-            if sense != 1:
-                rbs = self._invert_position(rbs, dna_length)
-            if rbs[0] > position[0]:
-                invalid_RBS.append(name)
-                rbs[0] = self._default_rba_start(position)
-            if rbs[0] < position[0] - 50:
-                long_RBS.append(name)
-                rbs[0] = self._default_rba_start(position)
-        rbs[1] = position[0] + 2
-        return rbs
+    def _invert_position(self, position):
+        return [self._dna_length - position[1] + 1,
+                self._dna_length - position[0] + 1]
 
-    def _is_undefined_rbs(self, rbs):
-        return rbs[0] == rbs[1] == 0
+    def _extract_rbs(self, row):
+        self.rbs_start = int(row[self._indices.rbs_start])
+        self.rbs_end = int(row[self._indices.rbs_end])
+        if not self.is_undefined_rbs() and self.is_antisense():
+            self.rbs_start, self.rbs_end = self._invert_position(
+                [self.rbs_start, self.rbs_end]
+                )
 
-    def _default_rba_start(self, position):
-        return position[0] - 21
+    def is_undefined_rbs(self):
+        return self.rbs_start == self.rbs_end == 0
+
+    def _extract_sequence(self, row):
+        self.sequence = row[self._indices.sequence].strip()
+        assert(len(self.sequence) == (self.end - self.start + 1))
+
+    def _extract_aa_seq(self, row):
+        self.aa_seq = row[self._indices.aa_seq].strip()
+        self.aa_composition = []
+        # we skip initial fMet
+        aa_seq = self.aa_seq[1:]
+        for aa in self.aas:
+            self.aa_composition.append(aa_seq.count(aa))
+
+    def _extract_TUs(self, row):
+        self.TUs = []
+        if row[self._indices.TU]:
+            self.TUs = map(str.strip, row[self._indices.TU].split(','))
+
+    def set_rbs_at_valid_location(self):
+        if (self.is_rbs_after_start_codon() or
+                self.is_rbs_too_far_before_stop_codon()):
+            self.set_rbs_at_default_location()
+
+    def set_rbs_at_default_location(self):
+        self.rbs_start = self.start - 21
+        self.rbs_end = self.start + 2
+
+    def is_rbs_after_start_codon(self):
+        return self.rbs_start > self.start
+
+    def is_rbs_too_far_before_stop_codon(self):
+        return self.rbs_start < self.start - 50
+
+    def is_usable(self):
+        return (self.has_valid_sequence_length() and
+                self.has_valid_stop_codon() and
+                not self.contains_premature_stop_codon() and
+                self.is_aa_sequence_consistent_with_sequence())
+
+    def has_valid_sequence_length(self):
+        return len(self.sequence) % 3 == 0
+
+    def has_valid_start_codon(self):
+        return is_valid_start_codon(self.sequence[:3])
+
+    def has_valid_stop_codon(self):
+        return is_valid_stop_codon(self.sequence[-3:])
+
+    def contains_premature_stop_codon(self):
+        nb_codons = int(len(self.sequence) / 3)
+        codons = [self.sequence[3*i:(3*i+3)] for i in range(nb_codons)]
+        return any(is_valid_stop_codon(c) for c in codons[:-1])
+
+    def is_aa_sequence_consistent_with_sequence(self):
+        return len(self.sequence) == 3*(len(self.aa_seq)+1)
+
+    def name_and_size(self):
+        return '{} [{}:{}]'.format(self.name, self.rbs_start, self.end)
 
 
-    def _is_valid_start_codon(self, sequence):
-        start_codon = sequence[:3]
-        return start_codon in ['atg', 'ctg', 'gtg', 'ttg']
+def is_valid_start_codon(codon):
+    return codon in ['atg', 'ctg', 'gtg', 'ttg']
 
-    def _is_valid_stop_codon(self, sequence):
-        stop_codon = sequence[-3:]
-        return stop_codon in ['taa', 'tag', 'tga']
 
-    def _is_valid_sequence(self, sequence):
-        # check sequence length
-        if len(sequence) % 3 != 0:
-            return False
-        # we do not count the stop codon here
-        nb_codons = len(sequence) / 3 - 1
-        codons = [sequence[3*i:(3*i+3)] for i in range(nb_codons)]
-        for c in codons:
-            if self._is_valid_stop_codon(c):
-                return False
-        return True
+def is_valid_stop_codon(codon):
+    return codon in ['taa', 'tag', 'tga']
