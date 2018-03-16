c20705f25de5c4f54acde9b3419001d5a8e00408
Stephan Fischer <yexilein@hotmail.com>
2018-03-16 13:16:39 +0100

    Correct bug in data extraction

--- simulations/gene_expression/scripts/post_treatment.R
+++ simulations/gene_expression/scripts/post_treatment.R
@@ -18,8 +18,8 @@ my_pdf = function (filename, w, h) {
 }
 
 data = load_replication(file.path(input_dir[1], data_dna));
-i = 1; while (data[[i]]$time <= max_time) { i = i+1; }
-data = data[[i-1]];
+i = 1; while (data[[i]]$time < max_time) { i = i+1; }
+data = data[[i]];
 min = data$sense[[1]][2]-10;
 max = data$sense[[1]][2]+1000;
 my_pdf('DNA_zoom.pdf'); plot_linear_DNA(data, min, max); dev.off();
