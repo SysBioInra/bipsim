45756f41eee9d2bd81648e7c3d9a83f50d2525ef
Stephan Fischer <yexilein@hotmail.com>
2018-03-16 12:20:09 +0100

    Adapt multifork script to new directory structure

--- simulations/scripts/run_multifork.sh
+++ simulations/scripts/run_multifork.sh
@@ -1,10 +1,9 @@
 
 #!/bin/sh
 
-SRC_PATH=/home/sfischer/simulateur/myBacteria/src
-SCRIPT_PATH=/home/sfischer/simulateur/simulations/scripts
 EXE_NAME=mybacteria
 LOG_FILE=log.txt
+SCRIPT_PATH=../scripts
 SCRIPT_NAME=multifork.R
 OUTPUT=output
 PARAMS_IN="input/params.in"
@@ -12,7 +11,7 @@ PARAMS_IN="input/params.in"
 rm -f $LOG_FILE
 for FILE in $PARAMS_IN
 do
-    ${SRC_PATH}/${EXE_NAME} ${FILE} 2>&1 | tee -a $LOG_FILE
+    ${EXE_NAME} ${FILE} 2>&1 | tee -a $LOG_FILE
 done
 
 # post treatment
