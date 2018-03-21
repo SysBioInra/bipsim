2d46db37a44b1ac7c61ba0fba9326e9c21d62878
Stephan Fischer <yexilein@hotmail.com>
2018-03-21 14:45:02 +0100

    Add violin plot for number of proteins per RNA

--- simulations/gene_expression/scripts/number_proteins.py
+++ simulations/gene_expression/scripts/number_proteins.py
@@ -13,7 +13,10 @@ from chemical_sequences import Proteins
 def main():
     stat_list = [ProteinStats(dir) for dir in sys.argv[1:]]
     number_vs_rnas(stat_list).savefig(
-        'number_proteins.pdf', bbox_inches='tight'
+        'number_vs_rnas.pdf', bbox_inches='tight'
+    )
+    protein_statistics(stat_list).savefig(
+        'protein_statistics.pdf', bbox_inches='tight'
     )
 
 
@@ -23,6 +26,15 @@ def number_vs_rnas(stat_list):
     for stats, marker in zip(stat_list, markers[:len(stat_list)]):
         plt.scatter(stats.rnas, stats.final_numbers,
                     facecolors='none', edgecolors='k', marker=marker)
+    return fig
+
+
+def protein_statistics(stat_list):
+    fig = plt.figure()
+    violin_pos = range(1, len(stat_list)+1, 1)
+    plt.violinplot([s.proteins_per_rna() for s in stat_list],
+                   violin_pos, points=100, widths=0.6,
+                   showmeans=True, showextrema=True, showmedians=True)
     return fig
 
 
@@ -36,6 +48,9 @@ class ProteinStats(object):
         self.ids = list(self.proteins.count.keys())
         self.rnas = list(self.proteins.count.values())
         self.final_numbers = [final_numbers[p] for p in self.ids]
+
+    def proteins_per_rna(self):
+        return [n / rna for n, rna in zip(self.final_numbers, self.rnas)]
 
 
 if __name__ == '__main__':
