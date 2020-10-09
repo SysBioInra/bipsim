c378004ff4cc46ef20941a63af495d0a878940f5
yexilein <stephischer@gmail.com>
2020-10-09 10:03:40 -0400

    Add paprameter to change script name (full output)

--- simulations/run_simulations.sh
+++ simulations/run_simulations.sh
@@ -1,13 +1,14 @@
 
 CURR_DIR=`pwd`
 LOG_FILE=${CURR_DIR}"/log_simulations.txt"
-#EXP_DIR="gene_expression/aggregated_constant gene_expression/aggregated_stacked gene_expression/hybrid_constant gene_expression/hybrid_stacked gene_expression/detailed_constant gene_expression/detailed_stacked gene_expression/detailed_detailed"
-EXP_DIR=paulsson/minimal
+EXP_DIR="gene_expression/aggregated_constant gene_expression/aggregated_stacked gene_expression/hybrid_constant gene_expression/hybrid_stacked gene_expression/detailed_constant gene_expression/detailed_stacked gene_expression/detailed_detailed"
+#EXP_DIR=paulsson/minimal
+SCRIPT_NAME=run.sh
 
 rm -f $LOG_FILE
 for DIR in $EXP_DIR
 do
     cd $DIR
-    sh run.sh 2>&1 | tee -a $LOG_FILE
+    sh $SCRIPT_NAME 2>&1 | tee -a $LOG_FILE
     cd $CURR_DIR
 done
