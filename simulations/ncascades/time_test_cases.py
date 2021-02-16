3aa3e5c1a10193580943584e19c292166e11c03d
yexilein <stephischer@gmail.com>
2021-02-16 11:21:27 -0500

    Improve CPU time measurement and adapt for bionetgen

--- simulations/ncascades/time_test_cases.py
+++ simulations/ncascades/time_test_cases.py
@@ -1,42 +1,38 @@
 
-from __future__ import absolute_import, division, print_function
 import sys
 import os
 import subprocess
+import time
 
 from src import test_case
 
 
 def main():
-    print('N I vector tree hybrid')
-    for case in test_case.read_file(sys.argv[1]):
-        vector_time = time_case(case.bipsim_directory("vector"))
-        tree_time = time_case(case.bipsim_directory("tree"))
-        hybrid_time = time_case(case.bipsim_directory("hybrid"))
+    case_file = "test_cases.txt"
+    
+    for case in test_case.read_file(case_file):
+        for method in ["ssa", "nf"]:
+            time_case(case.bionetgen_directory(method))
+        #for method in ["vector", "tree", "hybrid"]:
+        #    time_case(case.bipsim_directory(method))
         #copasi_time = time_case(case.copasi_directory())
-        print('{} {} {} {} {}'.format(case._length, case._initial_value,
-                                   vector_time, tree_time, hybrid_time))
 
 
 def time_case(directory):
     current_dir = os.getcwd()
     os.chdir(directory)
-    process = subprocess.Popen(['time sh run.sh'],
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
-    time_output = process.communicate()[1]
+    
+    start_time = time.time()
+    try:
+        result = subprocess.run(['sh', 'run.sh'], timeout = 7200)
+    except subprocess.TimeoutExpired:
+        print(directory + " timed out.")
+    end_time = time.time()
+    total_time = end_time-start_time
+    with open("time.txt", "w") as f:
+        f.write(str(total_time))
     os.chdir(current_dir)
-    return user_time_rugen_format(time_output)
 
-
-def user_time_old_format(output):
-    return float(output[:4])
-
-
-def user_time_new_format(output):
-    return float(output.strip().split()[2])
-
-def user_time_rugen_format(output):
-    return output.strip().split()[3]
-
+                    
 if __name__ == '__main__':
     main()
