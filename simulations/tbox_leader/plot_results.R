a2b438f37af4fde2c77be2df741a0ea7b61d6518
yexilein <stephischer@gmail.com>
2021-04-17 19:00:57 -0400

    Fix typo in T-box model

--- simulations/tbox_leader/plot_results.R
+++ simulations/tbox_leader/plot_results.R
@@ -1,5 +1,6 @@
 
 library(tidyverse)
+theme_set(theme_classic(base_size = 20))
 
 
 main = function() {
@@ -8,13 +9,25 @@ main = function() {
         select(-protein_tracker, -rna_tracker)
     results = results %>%
         pivot_longer(-time, names_to = "species", values_to = "number")
+    panel_a = c("EtRNA_Y_ca", "tRNA_Y", "Y")
     results %>%
-        ggplot(aes(x = time, y = number+1, col = species)) +
-        geom_line() +
-        theme_bw() +
-        scale_y_log10() +
-        labs(x="Time (s)", y = "# Molecules + 1")
-    ggsave("results.pdf")
+        filter(species %in% panel_a) %>%
+        ggplot(aes(x = time, y = number, col = species)) +
+        geom_line(size=2) +
+        geom_vline(xintercept=200, linetype="dashed") +
+        geom_vline(xintercept=600, linetype="dashed") +
+        labs(x="Time (s)", y = "# Molecules")
+    ggsave("results_a.pdf")
+
+    panel_b = c("aborted_U2301", "aborted_U2303", "U2301", "U2303", "tyrS_BSU29670")
+    results %>%
+        filter(species %in% panel_b) %>%
+        ggplot(aes(x = time, y = number, col = species)) +
+        geom_line(size=2) +
+        geom_vline(xintercept=200, linetype="dashed") +
+        geom_vline(xintercept=600, linetype="dashed") +
+        labs(x="Time (s)", y = "# Molecules")
+    ggsave("results_b.pdf")
 }
 
 read_results = function() {
