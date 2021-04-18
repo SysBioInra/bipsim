a3f0c59a1237423fd2457de84d4237b8d727790d
yexilein <stephischer@gmail.com>
2021-04-17 20:23:16 -0400

    Improve archive making script

--- simulations/dev/make_archive.sh
+++ simulations/dev/make_archive.sh
@@ -1,34 +1,42 @@
 # run this script on a freshly checked out repo to avoid copying results file etc.
 
-mkdir simulation_archive
+SIM_DIR=simulation-1.1
+mkdir $SIM_DIR
 
-cp README_archive.md simulation_archive/README.md
+cp README_archive.md $SIM_DIR/README.md
 
-mkdir simulation_archive/gene_expression
+mkdir $SIM_DIR/gene_expression
 EXP_DIR="aggregated_constant aggregated_stacked hybrid_constant hybrid_stacked detailed_constant detailed_stacked detailed_detailed"
 
 for DIR in $EXP_DIR
 do
-    mkdir simulation_archive/gene_expression/$DIR
-    mkdir simulation_archive/gene_expression/$DIR/output
-    cp -r ../gene_expression/$DIR/input simulation_archive/gene_expression/$DIR/
+    mkdir $SIM_DIR/gene_expression/$DIR
+    mkdir $SIM_DIR/gene_expression/$DIR/output
+    cp -r ../gene_expression/$DIR/input $SIM_DIR/gene_expression/$DIR/
 done
 
-mkdir simulation_archive/paulsson
-mkdir simulation_archive/paulsson/output
-cp -r ../paulsson/minimal/input simulation_archive/paulsson/
+mkdir $SIM_DIR/paulsson
+mkdir $SIM_DIR/paulsson/output
+cp -r ../paulsson/minimal/input $SIM_DIR/paulsson/
 
-mkdir simulation_archive/multifork
-mkdir simulation_archive/multifork/output
-cp -r ../multifork/input simulation_archive/multifork/
+mkdir $SIM_DIR/multifork
+mkdir $SIM_DIR/multifork/output
+cp -r ../multifork/input $SIM_DIR/multifork/
 
-cp -r ../guided_example simulation_archive/
-mkdir simulation_archive/guided_example/output
+cp -r ../guided_example $SIM_DIR/
+mkdir $SIM_DIR/guided_example/output
+mkdir $SIM_DIR/guided_example/output/annotations
+mkdir $SIM_DIR/guided_example/output/binding
+mkdir $SIM_DIR/guided_example/output/free_chemicals
+mkdir $SIM_DIR/guided_example/output/polymerization
+mkdir $SIM_DIR/guided_example/output/regulation
+mkdir $SIM_DIR/guided_example/output/sequences
 
-cp -r ../tbox_leader simulation_archive/
-mkdir simulation_archive/tbox_leader/output
+cp -r ../tbox_leader $SIM_DIR/
+mkdir $SIM_DIR/tbox_leader/output
 
-cp -r ../gupta_benchmark simulation_archive/
-cp -r ../ncascades simulation_archive/
+cp -r ../gupta_benchmark $SIM_DIR/
+cp -r ../ncascades $SIM_DIR/
 
-
+tar czf $SIM_DIR.tar.gz $SIM_DIR
+rm -rf $SIM_DIR
